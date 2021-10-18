4434dc768c1bc5b0f3b42d284e6085821be6ec15
Jiping Liu <jiping.liu@compass.com>
2021-10-18 22:58:49 +0800

    add attribute macros

--- rust-macros/proc_macro_examples/src/lib.rs
+++ rust-macros/proc_macro_examples/src/lib.rs
@@ -14,3 +14,11 @@ use proc_macro::TokenStream;
 pub fn make_answer(_item: TokenStream) -> TokenStream {
     "fn answer() -> u32 { 42 }".parse().unwrap()
 }
+
+#[proc_macro_attribute]
+pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("attr: \"{}\"", attr.to_string());
+    println!("item: \"{}\"", item.to_string());
+    item
+}
+
--- rust-macros/src/main.rs
+++ rust-macros/src/main.rs
@@ -1,7 +1,24 @@
 extern crate proc_macro_examples;
-use proc_macro_examples::make_answer;
+use proc_macro_examples::{make_answer, show_streams};
 
 make_answer!();
+
+// 基础函数
+#[show_streams]
+fn invoke1() {}
+
+// 带输入参数的属性
+#[show_streams(bar)]
+fn invoke2() {}
+
+// 输入参数中有多个token
+#[show_streams(multiple => tokens)]
+fn invoke3() {}
+
+#[show_streams { delimiters }]
+fn invoke4() {}
+
+
 
 fn main() {
     println!("{}", answer());
